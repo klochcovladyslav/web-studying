40bc2ec945f24018401fce2063f0b7cf43aa4ea2
klochcovladyslav <klochcovlad73@gmail.com>
2026-08-23 17:08:50 +0300

    aaaaand i tried to redesing site

--- js/burger-btn.js
+++ js/burger-btn.js
@@ -1,6 +1,11 @@
 let burgerBtn = document.querySelector('.burger-btn-box');
-let listBox = document.querySelector('.list-header-box')
+let listBox = document.querySelector('.list-header-box');
+let burgerSpans = document.querySelectorAll('.burger-span');
 
-burgerBtn.addEventListener('click', () => {
-    listBox.classList.toggle('list-header-box-toggle');
-}) 
+for (let i = 0; i < burgerSpans.length; i++) {
+    let burgerSpan = burgerSpans[i];
+    burgerBtn.addEventListener('click', () => {
+        listBox.classList.toggle('list-header-box-toggle');
+        burgerSpan.classList.toggle('burger-span-' + [i + 1] + '-toggle')
+    })
+}
